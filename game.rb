baf002f342e5e803d9ec728ecdced678b68421d5
Pascal Rey <pascalrey1955@gmail.com>
2021-12-08 17:48:51 -0600

    created tutorial file and added rough draft of monloge

--- game.rb
+++ game.rb
@@ -1,16 +1,18 @@
+require "./tutorial.rb"
+
 def intro
   puts "Would you like to enter? (yes/no)"
   answer = gets.chomp.downcase
 
   #start game
-  if answer == "yes"
+  if answer == "yes" or answer == "y"
     puts "You have chosen wisely."
     sleep(3)
     system "clear"
     start_game()
 
   #exit game
-  elsif answer == "no"
+  elsif answer == "no" or answer == "n"
     puts "You have chosen poorly."
     sleep(3)
     system "clear"
@@ -46,7 +48,7 @@ def start_game
     start_game()
   end
 end
-
+# Character select fucktion
 def char_select
   puts "What class would you like to select?"
   puts "Paladin (1)"
@@ -70,12 +72,12 @@ def char_select
         char_select()
     end
 end
-
+# Paladin select fucktion
 def paladin_select
   puts "The Paladin... A warrior of god, strong in conviction."
   puts "Paladins have the ability to temporarily increase 1 stat"
   puts "for 4 hours, this can be rejewed through prayer"
-  sleep (1)
+  sleep (3)
   puts ""
   puts "Stats for Paladin"
   puts "Perception: 7"
@@ -93,17 +95,108 @@ def paladin_select
   answer = gets.chomp
 
   case answer
-  when 1
+  when "1"
     @char_class = "paladin"
-    puts "good job you chose the worst class"
-    puts "please enter a name for your paladin"
+    puts "Good job, you chose the worst class"
+    puts "please enter a name for your Paladin."
     name = gets.chomp
-    puts "welcome to the order of the dragon #{name}"
-  when 2
+    puts "Welcome to the Order of the Dragon, #{name}."
+    tutorial_mission()
+  when "2"
     system "clear"
     char_select()
-  when 3
-    puts "¯\_(ツ)_/¯"
+  when "3"
+    puts "¯\\_(ツ)_/¯"
+    sleep(3)
+    system "exit"
+  else
+    puts "Input not recognized."
+    system "clear"
+    paladin_select()
+  end
+end
+# Ranger select fucktion
+def ranger_select
+  puts "The Ranger... He is both one with the forest, and with death"
+  puts "as he walks the Misty forest floor with his bow & arrow."
+  puts "Rangers have the ability to use Camouflage, and Feign Death"
+  sleep (3)
+  puts ""
+  puts "Stats for Ranger."
+  puts "Perception: 10"
+  puts "Strength: 6"
+  puts "Constitution: 8"
+  puts "Charisma: 1"
+  puts "Intelligence: 8"
+  sleep(3)
+  puts ""
+
+  puts "Select Ranger (1)"
+  puts "Return to character select (2)"
+  puts "go fuck yourself (3)"
+
+  answer = gets.chomp
+
+  case answer
+  when "1"
+    @char_class = "ranger"
+    puts "Good job, you chose the worst class"
+    puts "please enter a name for your Ranger."
+    name = gets.chomp
+    puts "Welcome to the The Hunters Guild, #{name}."
+    sleep(3)
+    system "clear"
+    tutorial_mission()
+  when "2"
+    system "clear"
+    char_select()
+  when "3"
+    puts "¯\\_(ツ)_/¯"
+    sleep(3)
+    system "exit"
+  else
+    puts "Input not recognized."
+    system "clear"
+    paladin_select()
+  end
+end
+# Diplomat select fucktion
+def diplomat_select
+  puts "The Diplomat... He is famous for being extremely skilled in speech."
+  puts "Rarely will you ever find the Diplomat on someone's bad side."
+  puts "Diplomats are given more conversation options in game."
+  sleep (3)
+  puts ""
+  puts "Stats for Diplomat."
+  puts "Perception: 1"
+  puts "Strength: 2"
+  puts "Constitution: 6"
+  puts "Charisma: 10"
+  puts "Intelligence: 7"
+  sleep(3)
+  puts ""
+
+  puts "Select Diplomat (1)"
+  puts "Return to character select (2)"
+  puts "go fuck yourself (3)"
+
+  answer = gets.chomp
+
+  case answer
+  when "1"
+    @char_class = "diplomat"
+    puts "Good job, you chose the worst class"
+    puts "please enter a name for your Diplomat."
+    name = gets.chomp
+    puts "Welcome to the Enclave of the Architects, #{name}."
+    sleep(3)
+    system "clear"
+    tutorial_mission()
+  when "2"
+    system "clear"
+    char_select()
+  when "3"
+    puts "¯\\_(ツ)_/¯"
     sleep(3)
     system "exit"
   else
---
+++ tutorial.rb
@@ -0,0 +1,15 @@
+def tutorial_mission
+  tutorial_monologue()
+end
+
+def tutorial_monologue
+  weapon = "Glaive"
+  puts "Your bag falls off the seat from the bumps on the road."
+  puts "The carriage driver calls back in old gruff voice \"we'll"
+  puts "be there soon, gather your belongings I dont have time"
+  puts "for a long stop\". as you gather your #{weapon} and your"
+  puts "bag. Your employer said he got you a room at the Misty Step."
+  puts "The idea of having to hunt something down in town sounds pretty"
+  puts "shit but given the amount of money on the table it's quite hard"
+  puts "to say no."
+end
